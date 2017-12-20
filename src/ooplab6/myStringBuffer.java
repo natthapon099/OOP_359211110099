839cfa7a10caa2d453eb8e259231a43accded7dc
natthapon099 <nat.trairat2538@gmail.com>
2017-12-20 11:37:21 +0700

    mystringbuffer

---
+++ src/ooplab6/myStringBuffer.java
@@ -0,0 +1,23 @@
+package ooplab6;
+
+
+
+public class myStringBuffer {
+    public static void main(String[] args) {
+        //StringBuffer
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Hello");
+        System.out.println (buffer);
+        buffer.insert(5," ");
+        //System.out.println(buffer.reverse());
+        buffer.append("RMUTSV");
+        System.out.println(buffer);
+        buffer.delete(6, 8);
+        System.out.println(buffer);
+        pringBuffer(buffer);
+
+    }
+
+    private static void pringBuffer(StringBuffer buffer) {
+    }
+}
