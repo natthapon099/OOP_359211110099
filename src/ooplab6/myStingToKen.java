7235087ba16466f63bf62b815ba9c436901fc4d9
natthapon099 <nat.trairat2538@gmail.com>
2017-12-20 11:52:54 +0700

    mystringbuffer

--- src/ooplab6/myStingToKen.java
+++ src/ooplab6/myStingToKen.java
@@ -1,4 +1,21 @@
 package ooplab6;
 
-public class myStingToKen {
-}
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class myStringToken {
+    public static void main(String[] args) {
+        String s = "Welcome to RMUTV";
+        StringTokenizer tokenizer = new StringTokenizer(s);
+        //count word in String
+        System.out.println(tokenizer.countTokens());
+        //cut word in String
+        while (tokenizer.hasMoreTokens()){
+            StringBuffer buffer = new StringBuffer(tokenizer.nextToken());
+            System.out.println(buffer.reverse());
+        }
+
+    }//maian
+}//class
+
