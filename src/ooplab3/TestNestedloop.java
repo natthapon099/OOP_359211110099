b5ee15b3aca771a4d91b7e84218efcdf309326d1
natthapon099 <nat.trairat2538@gmail.com>
2017-12-20 09:42:17 +0700

    TestLoopfor

---
+++ src/ooplab3/TestNestedloop.java
@@ -0,0 +1,46 @@
+package ooplab3;
+
+public class TestNestedloop {
+    public static void main(String[] args) {
+        int n=1;
+        for (int i=1;i<=8;i++){
+            //statement
+            for(int j=1;j<=i;j++){
+                //statement
+                if(i==8||i==j||j==1) {
+                    System.out.print("*\t");
+                }else {
+                    System.out.print(n + "\t");
+                    n++;
+                }
+            }//j
+            System.out.println();
+
+        }//i
+        System.out.println("\t\t");
+        int x=1;
+        int y = 15;
+        for (int i=1;i<=8;i++){
+            //statement
+            for(int j=1;j<=8;j++){
+                //statement
+                if(i==8||i==j||j==1||j==8||i==1) {
+                    System.out.print("*\t");
+                }else if(i<j){
+                    System.out.print(y + "\t");
+                    y--;
+                }else {
+                    System.out.print(x+"\t");
+                    x++;
+
+                }
+            }//j
+            System.out.println();
+
+        }//i
+
+
+    }//main
+
+
+}// class
