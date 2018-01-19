c9366a690ab5e9f66d2368b00bb70a0e6e1824d7
natthapon099 <nat.trairat2538@gmail.com>
2018-01-20 00:51:39 +0700

    Test Edit Data

--- src/ooplab6/myStingToKen.java
+++ src/ooplab6/myStingToKen.java
@@ -1,21 +1,21 @@
 package ooplab6;
 
-import javax.swing.plaf.basic.BasicButtonUI;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
-public class myStringToken {
+public class myStingToKen {
     public static void main(String[] args) {
-        String s = "Welcome to RMUTV";
-        StringTokenizer tokenizer = new StringTokenizer(s);
-        //count word in String
-        System.out.println(tokenizer.countTokens());
-        //cut word in String
-        while (tokenizer.hasMoreTokens()){
-            StringBuffer buffer = new StringBuffer(tokenizer.nextToken());
-            System.out.println(buffer.reverse());
-        }
+            String s = "Welcome to RMUTSV";
+            StringTokenizer tokenizer = new StringTokenizer(s);
+            //count word in String
+            System.out.println(tokenizer.countTokens());
+            //cut word in String
+            //reverse word in String
+            while (tokenizer.hasMoreTokens()){
+                StringBuffer buffer = new StringBuffer(tokenizer.nextToken());
+                System.out.println(buffer.reverse());
+            }
 
-    }//maian
-}//class
+        }//main
+    }//class
 
---
+++ src/ooptest/HelloWorld.java
@@ -0,0 +1,13 @@
+package ooptest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world,I'm NAT");
+        //  Add Data
+        System.out.println("natthapon");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology,RMUTSV");
+
+    }//main
+}//class
+
