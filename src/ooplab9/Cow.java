a383ee038c7782c6e8dc162733179aefe07e1a57
natthapon099 <nat.trairat@gmail.com>
2018-02-26 23:27:40 +0700

    Cow

---
+++ src/ooplab9/Cow.java
@@ -0,0 +1,16 @@
+package ooplab9;
+
+import ooplab8.Pet;
+
+public class Cow {
+    public Cow(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("More More !!!");
+    }
+}
+
+
