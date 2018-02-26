6299815b355588384ebdb500a05e8580775cf91f
natthapon099 <nat.trairat@gmail.com>
2018-02-27 00:14:01 +0700

    ShapeApp

--- src/ooplab8/ShapeApp.java
+++ src/ooplab8/ShapeApp.java
@@ -1,8 +1,6 @@
 package ooplab8;
 
 public class ShapeApp {
-    public class ShapeApp {
-    }
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(20, 30);
         System.out.println(rectangle.toString());
