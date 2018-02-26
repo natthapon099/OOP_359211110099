345b54ccaeeaa2398fc670a2c32ea2db0ac508d0
natthapon099 <nat.trairat@gmail.com>
2018-02-26 23:05:36 +0700

    Dog

---
+++ src/ooplab8/Person.java
@@ -0,0 +1,61 @@
+package ooplab8;
+
+public class Person {private String name;
+    private String gender;
+    private String dateOfBirth;
+    private int age;
+    //constructor
+    public Person(String name, String gender, String dateOfBirth, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+    //toString()
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", age=" + age +
+                '}';
+    }
+
+    //getter and setter methods
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}//class
+
